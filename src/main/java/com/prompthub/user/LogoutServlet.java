d33f35a97c7d09dc18a86d97113b0b3261e7a5f5
dmstj0162 <dmstj0162@gmail.com>
2026-05-29 14:50:03 +0900

    로그인, 로그아웃 프론트 연결 기능 완료

---
+++ src/main/java/com/prompthub/user/LogoutServlet.java
@@ -0,0 +1,29 @@
+package com.prompthub.user;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/auth/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {{
+
+            HttpSession session = req.getSession(false);
+
+            if(session != null) {
+                session.invalidate();
+            }
+
+            System.out.println("[Logout] Completed Logout");
+
+            resp.getWriter().write("LOGOUT_SUCCESS");
+        }
+    }
+}
